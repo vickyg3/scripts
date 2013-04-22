7729ce3511b136b656c1938ce7177631ceb8fc3c
Vignesh <vigneshacitizenofindia@gmail.com>
2013-04-22 00:23:48 -0700

    Adding own trie implementation
    removing unwanted files

--- efficient_bot.py
+++ efficient_bot.py
@@ -4,13 +4,79 @@
 
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 import time
-import trie
+import optparse
 
-words = [word.strip() for word in open('dictionary.txt', 'r')]
+# classes for trie
+
+def o(c):
+    return ord(c) - 97
+
+class Node(object):
+    def __init__(self):
+        self.words = []
+        self.children = [None] * 26
+        self.path = [False] * 26
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = Node()
+
+    def add(self, word):
+        node = self.root
+        for index, char in enumerate(word):
+            if index < len(word) - 1:
+                if not node.children[o(char)]:
+                    node.children[o(char)] = Node()
+                node = node.children[o(char)]
+            else:
+                node.words.append(word)
+                node.path[o(char)] = True
+
+    def exists(self, word):
+        node = self.root
+        for index, char in enumerate(word):
+            if index == len(word) - 1 and node.path[o(char)]:
+                return True
+            elif index == len(word) - 1 or not node.children[o(char)]:
+                return False
+            node = node.children[o(char)]
+
+    def exists_prefix(self, prefix):
+        node = self.root
+        # handle single length prefixes
+        if len(prefix) == 1:
+            return bool(node.children[o(prefix)])
+        # prefixes length > 1
+        for index, char in enumerate(prefix):
+            if index == len(prefix) - 1:
+                return bool(sum([bool(child) for child in node.children]))
+            if not node.children[o(char)]:
+                return False
+            node = node.children[o(char)]
+
+# end of trie classes
+
+# options parsing
+parser = optparse.OptionParser()
+parser.add_option('-s', '--start-from-length', action = "store", 
+                  dest = "start_from_length", type = "int", default = 5,
+                  help = "Start outputting numbers with length >= this number first")
+parser.add_option('-r', '--no-reverse', action = "store_true", 
+                  dest = "no_reverse", default = False,
+                  help = "Do not reverse the small words list")
+parser.add_option('-f', '--dictionary-file', action = "store",
+                  dest = "dictionary_file", default = "dictionary.txt",
+                  help = "Path to dictionary file")
+opts, args = parser.parse_args()
+
+words = [word.strip() for word in open(opts.dictionary_file, 'r')]
 print 'constructing data structure'
-word_trie = trie.Trie()
+start_time = time.time()
+trie = Trie()
 for word in words:
-    word_trie[word] = 1
+    trie.add(word)
+print 'trie constructed in', time.time() - start_time, 'seconds'
 print 'done'
 
 print 'waiting for device'
@@ -19,7 +85,7 @@ print 'device attached'
 
 row_coordinates = (
                     (28, 292, 748, 184),
-                    (28, 478, 748, 184)
+                    (28, 478, 748, 184),
                     (28, 668, 748, 184),
                     (28, 852, 748, 184)
                   )
@@ -36,7 +102,7 @@ cnt = 0
 small_words = []
 freezes = [0, 0, 0]
 
-found_words = trie.Trie()
+found_words = Trie()
 
 def getx(y):
 	return 200 * y + 100;
@@ -67,28 +133,22 @@ def tuple_to_string(word):
     return ''.join([actual_word for actual_word, d1, d2 in word])
 
 def process(word):
-    global cnt, q, device, start_time, small_words
+    global cnt, q, device, start_time, small_words, opts
     string = tuple_to_string(word)
     # string should be a valid word of length atleast 4 and hasn't been seen already
     add_word = True
-    try:
-        dummy = word_trie[string] # if this fails, then word not found 
-    except trie.NeedMore:
-        add_word = True
-    except:
-        add_word = False
-    else:
-        try:
-            dummy = found_words[string]
-        except:
-            found_words[string] = 1
-            if(len(string) > 4):
+    if trie.exists(string):
+        if not found_words.exists(string):
+            found_words.add(string)
+            if(len(string) >=  opts.start_from_length):
                 print cnt, ":", string
                 print time.time() - start_time
                 emulate(word)
                 cnt += 1
             elif len(string) > 1:
                 small_words.append(word[:])
+    elif not trie.exists_prefix(string):
+        add_word = False
     if add_word:
         q.append(word)
 
@@ -124,7 +184,9 @@ while len(q) > 0:
             head = old_head
 
 # output all the small words
-for small_word in reversed(small_words):
+if not opts.no_reverse:
+    small_words.reverse()
+for small_word in small_words:
     emulate(small_word)
     print cnt, ":", tuple_to_string(small_word)
     print time.time() - start_time
