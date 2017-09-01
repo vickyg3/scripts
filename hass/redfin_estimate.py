d97a69848a508a299c8e79d9693dd142abe1d531
Vignesh Venkatasubramanian <vigneshacitizenofindia@gmail.com>
2017-09-01 12:49:47 -0700

    hass redfin estimate

---
+++ hass/redfin_estimate.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+import requests
+import sys
+import re
+
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+  redfin_url = 'https://www.redfin.com/CA/Santa-Clara/3003-Monroe-St-95051/home/1294779'
+  user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
+  headers = {'User-Agent': user_agent}
+  try:
+    soup = BeautifulSoup(requests.get(redfin_url, headers=headers).text, 'html.parser')
+    redfin_estimate = soup.find('div', {'data-rf-test-id': 'avm-price'}).find('div').text
+    estimate = re.match("\$([0-9]+),([0-9]+),.*", redfin_estimate).groups()
+    print "%s.%sM" % (estimate[0], estimate[1])
+  except:
+    raise
+    print 'N/A'
