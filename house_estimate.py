617c8e426b29a5b81c262230e1969e8028d591f2
Vignesh Venkat <vigneshacitizenofindia@gmail.com>
2020-05-17 02:07:04 -0700

    include michigan home

--- house_estimate.py
+++ house_estimate.py
@@ -41,17 +41,17 @@ if __name__ == "__main__":
   redfin_urls = [
       'https://www.redfin.com/CA/Santa-Clara/3003-Monroe-St-95051/home/1294779',
       'https://www.redfin.com/CA/Fremont/43474-Newport-Dr-94538/home/527548',
-      'https://www.redfin.com/CA/Santa-Clara/3321-Pruneridge-Ave-95051/home/737513'
-  ]
+      'https://www.redfin.com/CA/Santa-Clara/3321-Pruneridge-Ave-95051/home/737513',
+      'https://www.redfin.com/MI/Troy/4621-Bradley-Cir-48085/home/113431822']
   zws_id = 'X1-ZWz196sfugrtaj_6a5jo'
-  zpids = ['19552594', '25041322', '19598205']
+  zpids = ['19552594', '25041322', '19598205', '2087627195']
   user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
   headers = {'User-Agent': user_agent}
   spreadsheetIds = [
       '1BJ7cUThSWkcNYWdzmr9qW9MOHDVD2qWBl79dk7gUSZg',
       '1VkUrhI5i4CfnvKJmLAiwaYlGaHiOhK_ieQiHQ4hIwtk',
-      '1rOrigbqCd68_QdDS-oWWiMWx2PPraIPCTOvPaPsS9nk'
-  ]
+      '1rOrigbqCd68_QdDS-oWWiMWx2PPraIPCTOvPaPsS9nk',
+      '1XuDD2O7_-UKZSAguuJjAfSOQtY575UvOQmmWJCeO0DQ']
   for i in range(len(redfin_urls)):
     redfin_url = redfin_urls[i]
     zpid = zpids[i]
